a4d26c072118b9add2eafe7f3f5f48f12dec0073
jeremiah-carlson <carlsonjeremiah2@gmail.com>
2023-08-20 13:09:16 -0400

    Add Clock

---
+++ www/src/components/Clock.jsx
@@ -0,0 +1,28 @@
+import { createSignal, onCleanup } from "solid-js";
+
+function getCurrentStrfTime() {
+    let date = new Date();
+    let out = `${date.getHours()}:${('0' + date.getUTCMinutes()).slice(-2)}:${('0' + date.getUTCSeconds()).slice(-2)}`;
+    return out;
+};
+
+
+const Clock = ()=>{
+    const [clockTime, setClockTimes] = createSignal(
+        getCurrentStrfTime()
+    );
+
+    const timer = setInterval(()=>{
+        setClockTimes(getCurrentStrfTime());
+    }, 1000);
+
+    onCleanup(()=>clearInterval(timer));
+
+    return (
+        <>
+        <h1 class="text-slate-100">{clockTime()}</h1>
+        </>
+    );
+};
+
+export default Clock;
