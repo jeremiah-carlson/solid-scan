36aaa2365cc31aac0e37c44e93358a8617507a93
jeremiah-carlson <carlsonjeremiah2@gmail.com>
2023-09-04 21:30:44 -0400

    Input autocomplete off

--- www/src/components/MainInput.jsx
+++ www/src/components/MainInput.jsx
@@ -11,7 +11,7 @@ const MainInput = ()=>{
 
     return (
         <div class="w-1/2 h-0 overflow-hidden">
-            <input id="MainInput" onBlur="this.focus()" class="w-full h-16 rounded-md p-4 text-2xl" value={mainInputVal()}></input>
+            <input id="MainInput" onBlur="this.focus()" autocomplete="off" class="w-full h-16 rounded-md p-4 text-2xl" value={mainInputVal()}></input>
             <input type='submit' hidden></input>
         </div>
         
