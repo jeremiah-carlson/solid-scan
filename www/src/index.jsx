8968b4028a2015865d3be466711fbb0d31803dfc
jeremiah-carlson <carlsonjeremiah2@gmail.com>
2023-09-13 01:43:16 -0400

    Auto generate paths

--- www/src/index.jsx
+++ www/src/index.jsx
@@ -1,9 +1,9 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { Switch, render } from 'solid-js/web';
 import { Router, Route, Routes, hashIntegration } from "@solidjs/router";
 
 import './index.css';
-import { Concert, Default } from './pages/Production';
+import * as Page from './pages/Production';
 
 const root = document.getElementById('root');
 
@@ -14,12 +14,18 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
 }
 
 const routerSrc = (["hash", "HASH"].includes(import.meta.env.VITE_ROUTING))? hashIntegration() : null;
+const rootPage = import.meta.env.VITE_ROOT_PAGE ?? "default"
 
 render(() =>  (
     <Router source={routerSrc}>
       <Routes>
-        <Route path="/" component={Default} />
-        <Route path="/concert/" component={Concert} />
+        <For each={Object.entries(Page)}>{
+          (pg, i)=> <Switch fallback={<Route path={`/${pg[0].toLowerCase()}`} component={pg[1]}/>}>
+                      <Match when={pg[0].toLowerCase() == rootPage}>
+                        <Route path="/" component={pg[1]}/>
+                      </Match>
+                    </Switch>
+        }</For>
       </Routes>
     </Router>
   ),
