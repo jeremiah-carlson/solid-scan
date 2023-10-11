5203155e813671f7b5a8a5305e59691b830b4f44
Parker Gagliano <pjg4086@uncw.edu>
2023-10-11 15:54:30 -0400

    improve grid

--- www/src/components/QR.jsx
+++ www/src/components/QR.jsx
@@ -20,10 +20,11 @@ const QR = (props)=>{
   });
 
   return (
-    <div class="flex flex-col justify-center align-middle relative w-full h-full">
-    <h1 class="text-slate-700 text-center w-full font-semibold text-lg z-[900] absolute left-0 top-0">{props.label}</h1>
-    <a href="#"  onDblClick={[qrClick, props.qrString]} class='z-200 absolute left-0 top-0 w-full text-align-center' innerHTML={qrUrl}/>
-    </div>
+    <div class="flex items-center flex-col justify-center relative w-full h-full">
+    <h1 class="text-slate-200 text-center w-full font-semibold text-xs lg:text-base xl:text-xl z-[900] overflow-hidden  left-0 top-0">{props.label}</h1>
+    <a href="#" onDblClick={[qrClick, props.qrString]} class="z-200 w-full text-center" innerHTML={qrUrl}/>
+</div>
+
   );
 };
 
